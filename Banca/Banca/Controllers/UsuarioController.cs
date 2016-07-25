b4bf28ac132517aac1f7fa69a5ed33d69a557cb4
Jimmy Muchachasoy <jimmy.muchachasoy@techandsolve.com>
2016-07-25 12:09:53 -0500

    aplicacion csharp

---
+++ Banca/Banca/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using Banca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Banca.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        Usuario usuario;
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Banca/Banca/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Banca.Models
+{
+    public class Usuario
+    {
+        public string nombre {get;set;}
+        public string password { get; set; }
+    }
+}
